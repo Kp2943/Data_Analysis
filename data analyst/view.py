7fceafd04f6845d3e38b813e229f2aa9741edf5f
Priyansh Verma <114761559+Kp2943@users.noreply.github.com>
2024-12-18 16:10:12 +0530

    Add files via upload

---
+++ data analyst/view.py
@@ -0,0 +1,22 @@
+# ***View() function***
+
+'''
+1. It is an inbuilt function here both the arrays have different address 
+2. Even though different add. arr present modification will happen in both the arrays
+
+> Syntax :-   arr1.view()
+'''
+
+# ***Operations on arrays***
+
+'''
+1. Array with array
+2. Array with constant / scalar
+3. Inbuilt functions on array
+'''
+
+# ***Array with array ***
+
+'''
+1. to do array operation min we need 2 arrays
+2. both the arrays should be same length
